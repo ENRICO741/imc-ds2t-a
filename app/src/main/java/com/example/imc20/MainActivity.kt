9ed316f7c4d2bfb6c1cbb7b95009b19143d23ab9
ENRICO MARÇOLA FEITOZA <21193365@CFP127ALUNO.LOCAL>
2021-09-13 16:44:28 -0300

    aula13

--- app/src/main/java/com/example/imc20/MainActivity.kt
+++ app/src/main/java/com/example/imc20/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +13,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val editPeso = findViewById<EditText>(R.id.edit_peso)
+        val editAltura = findViewById<EditText>(R.id.edit_altura)
         val buttonCalcular = findViewById<Button>(R.id.button_calcular)
+        val textViewResultado = findViewById<TextView>(R.id.text_view_resultado)
 
         buttonCalcular.setOnClickListener {
-            Toast.makeText(this, editPeso.text.toString(), Toast.LENGTH_SHORT).show()
+
+            if (editPeso.text.isEmpty()){
+                editPeso.error = "Peso é obrigatório!"
+            } else if (editAltura.text.isEmpty()){
+                editAltura.error = "Altura é obrigatório!"
+            } else {
+
+                val peso = editPeso.text.toString().toInt()
+                val altura = editAltura.text.toString().toDouble()
+
+                val imc = peso / (altura * altura)
+
+                textViewResultado.text = String.format("%.2f", imc)
+            }
         }
 
     }
